4ac06a76f99e00c7cbaa5ee003e665f24dcec233
melimisa <isabela.pereiramelim@gmail.com>
2020-10-05 00:19:26 -0300

    Criação da classe Disciplina.java

---
+++ Avaliacao1/src/java/avaliacao1/Disciplina.java
@@ -0,0 +1,63 @@
+package avaliacao1;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isame
+ */
+public class Disciplina {
+    private String nome;
+    private String ementa;
+    private Double nota;
+    private int ciclo;
+
+    public Disciplina(String nome, String ementa, int ciclo) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+    
+    public static ArrayList<Disciplina> gotList(ArrayList<Disciplina> disciplina){
+        ArrayList<Disciplina> minhasDisciplinas = disciplina;
+        return minhasDisciplinas;
+    }
+}
